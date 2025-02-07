bf127e9b20b421732336641474779d10ce856df4
Aljosa Lojpur <aljosalojpur@gmail.com>
2025-02-07 18:48:56 +0100

    Add login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,38 @@
-import { useEffect } from 'react';
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 
 const Home = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const router = useRouter();
 
-  useEffect(() => {
-    router.push('/following');
-  }, [router]);
+  const handleLogin = () => {
+    if (username && password) {
+      localStorage.setItem('user_id', username);
+      router.push('/following');
+    } else {
+      alert('Please enter a username and password.');
+    }
+  };
 
-  return null;
+  return (
+    <div className="login-container">
+      <h1>Login</h1>
+      <input
+        type="text"
+        placeholder="Enter your username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Enter your password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
 };
 
 export default Home; 
